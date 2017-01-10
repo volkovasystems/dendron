6e8ac981e382c3f5157862dd448e40da451f3777
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-10 16:43:08 +0800

    Update test.

--- test.js
+++ test.js
@@ -1,3 +1,50 @@
+"use strict";
+
 const dendron = require( "./dendron.js" );
 
-console.log( dendron( { "name": "Merchant" } ) )
+const diatom = require( "diatom" );
+const heredito = require( "heredito" );
+const optcall = require( "optcall" );
+
+const Computation = diatom( "Computation" );
+const MerchantCompute = diatom( "MerchantCompute" );
+
+Computation.prototype.initialize = function initialize( option, callback ){
+
+    return this;
+
+};
+
+
+Computation.prototype.compute = function compute( option, callback ) {
+	console.log( "compute" );
+
+    callback( null, null, option );
+
+    return this;
+
+};
+
+Computation.prototype.applySomeThing = function applySomeThing( option, callback ) {
+
+    console.log( "apply something", option );
+
+    callback( null, null, option );
+
+    return this;
+
+};
+
+Computation.prototype.done = function done( option, callback ) {
+
+    console.log( "done" );
+
+	callback( null, null, option );
+
+    return this;
+
+};
+
+optcall( Computation );
+
+heredito( MerchantCompute, Computation );
