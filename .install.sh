36d25b68000ea124dd2150ad6c98c18bd2730856
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-31 13:22:19 +0800

    Updated dependencies.

---
+++ .install.sh
@@ -0,0 +1,28 @@
+yarn add \
+ate \
+called \
+cobralize \
+diatom \
+doubt \
+falze \
+fnord \
+harden \
+hashids \
+heredito \
+llamalize \
+lodash \
+loosen \
+node-uuid \
+olivant \
+optcall \
+petrifi \
+protype \
+raze \
+shardize \
+silph \
+snapd \
+symbiote \
+tinge \
+truu \
+u200b \
+--save
